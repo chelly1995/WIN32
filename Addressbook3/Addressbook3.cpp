b9c4cb56b1fcc030d09f7ed460c35a16fb11104a
chelly1995 <92913531+chelly1995@users.noreply.github.com>
2023-05-08 17:27:44 +0900

    Add files via upload

--- Addressbook3/Addressbook3.cpp
+++ Addressbook3/Addressbook3.cpp
@@ -43,7 +43,8 @@ int main()
 		scanf_s("%d", &num);
 
 
-		if (num == 1) {
+		if (num == 1)
+		{
 
 
 			printf("이름 : ");
@@ -57,29 +58,111 @@ int main()
 			
 			
 			
-			printf(" \n 주소록이 추가되었습니다. \n");
+			printf(" \n 주소록이 추가되었습니다. \n\n");
 
 
 			Address_Num++;
 
 		}
-		else if (num == 2) {
-			int delete_num;
-			printf("삭제할 번호를 입력해주세요 : ");
-			scanf("%d \n",&delete_num);
+		else if (num == 2) 
+		{
+			//int delete_num;
+			
+			char name[10];	// 삭제할 이름 저장
 
-			for (int i = delete_num; i < Address_Num-1; i++)
+			printf("삭제할 이름을 입력해주세요 : ");
+			scanf("%s",&name);
+
+			for (int i = 0; i < Address_Num-1; i++)
 			{
-				AddList[i] = AddList[i + 1];
+				//AddList[i] = AddList[i + 1];
+
+				AddList[i].age = AddList[i + 1].age;
+				strcpy(AddList[i].name,AddList[i+1].name);
+				strcpy(AddList[i].address,AddList[i+1].address);
+
+
+				Address_Num--;
 			}
 
 			
 
-			printf("%d번이 삭제되었습니다. \n",&delete_num);
+			//printf("%d번이 삭제되었습니다. \n",&delete_num);
 	
 			Address_Num--;
 		}
-		else if (num == 5) {
+		else if (num == 3)
+		{
+			char name[10]; // 검색을 원하는 사원이름 저장
+			char temp[20];	// 주소 입력
+			int cmd = 0;
+			int temp_age;	// 나이 입력
+
+			printf(" 수정할 사람의 이름 : ");
+			scanf("%s",&name);
+
+			for (int i = 0; i < Address_Num; i++) {
+			
+				for (int i = 0; i < Address_Num; i++) {
+					if (strcmp(AddList[i].name, name) == 0) {
+						// char[]의 비교는 strcmp함수 사용
+
+						printf("[%d] 이름 : %s 나이 : %d, 주소 : %s \n", i + 1, AddList[i].name, AddList[i].age, AddList[i].address);
+
+						printf("수정 목록을 입력해주세요. (1. 나이 2. 주소) \n");
+						scanf("%d",&cmd);
+					
+						if (cmd == 1) {
+								
+							printf("수정 나이 : ");
+							scanf("%d",&temp_age);
+							AddList[i].age = temp_age;
+
+
+						}
+						else if (cmd == 2) {
+						
+							printf("수정 주소 : ");
+							scanf("%s", &temp);
+							strcpy(AddList[i].address,temp);
+				
+				
+						}
+					
+					}
+
+
+				}
+			
+			}
+
+		}
+		else if (num == 4)
+		{
+			char name[10];	// 검색을 원하는 사원이름 저장
+
+			printf("검색 이름 : ");
+			scanf("%s",&name);
+
+			for (int i = 0; i < Address_Num; i++) {
+				if (strcmp(AddList[i].name, name) == 0) {
+					// char[]의 비교는 strcmp함수 사용
+
+					printf("[%d] 이름 : %s 나이 : %d, 주소 : %s \n", i + 1, AddList[i].name, AddList[i].age, AddList[i].address);
+				
+				
+				}
+				else {
+					printf(" \n 검색 결과가 없습니다.\n\n");
+				
+				}
+			
+			
+			}
+
+		}
+		else if (num == 5) 
+		{
 
 			printf("\n =============== 전체 리스트 ==============\n\n");
 
